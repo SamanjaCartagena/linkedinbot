bc83b2a938f989e3fe897be1c99a233d516b6496
Samanja Cartagena <c.samanja09@Pabs.local>
2023-09-27 01:19:15 -0400

    automation

--- linked.py
+++ linked.py
@@ -12,10 +12,10 @@ time.sleep(3)
 email=driver.find_element(By.ID,"session_key")
 password = driver.find_element(By.ID,"session_password")
 submit = driver.find_element(By.XPATH,"/html[1]/body[1]/main[1]/section[1]/div[1]/div[1]/form[1]/div[2]/button[1]")
-email.send_keys("")
-password.send_keys("")
+email.send_keys("cart.samanja09@gmail.com")
+password.send_keys("Sam#Carta32")
 submit.click()
-time.sleep(3)
+time.sleep(10)
 search=driver.find_element(By.XPATH,"//input[@placeholder='Search']")
 search.send_keys("real estate",Keys.ENTER)
 time.sleep(3)
@@ -23,8 +23,26 @@ people=driver.find_element(By.XPATH,"//button[text()='People']")
 time.sleep(3)
 people.click()
 time.sleep(5)
-connect=driver.find_element(By.XPATH,"//a[contains(text(), 'Connect')]")
-time.sleep(3)
-connect.click()
+for x in range(5):
+ connect=driver.find_element(By.XPATH,"//span[text()='Connect']")
+#connect=driver.find_element(By.XPATH,"/html[1]/body[1]/div[4]/div[3]/div[2]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[x]/div[1]/div[1]/div[3]/div[1]/button[1]/span[1]")
+ time.sleep(3)
+ connect.click() 
+ time.sleep(3)
+ driver.execute_script("document.activeElement")
+ time.sleep(3)
+ send=driver.find_element(By.XPATH,"//button[@aria-label='Send now']")
+ time.sleep(3)
+ send.click()
+ time.sleep(3)
+ 
+driver.execute_script("document.activeElement")
+
 driver.quit()
 print("Automation Complete") 
+
+
+
+
+
+
