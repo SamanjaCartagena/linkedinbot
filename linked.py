ab0fc72e0b1e3e893e695ebd3ce96e2d370870f8
Samanja Cartagena <c.samanja09@Pabs.local>
2023-10-03 02:15:01 -0400

    works

--- linked.py
+++ linked.py
@@ -17,9 +17,9 @@ submit = driver.find_element(By.XPATH,"/html[1]/body[1]/main[1]/section[1]/div[1
 email.send_keys("cart.samanja09@gmail.com")
 password.send_keys("Sam#Carta32")
 submit.click()
-time.sleep(5)
+time.sleep(20)
 search=driver.find_element(By.XPATH,"//input[@placeholder='Search']")
-search.send_keys("real estate",Keys.ENTER)
+search.send_keys("software engineer",Keys.ENTER)
 time.sleep(3)
 
 driver.find_element(By.XPATH,"//button[text()='People']")
@@ -27,59 +27,53 @@ people=driver.find_element(By.XPATH,"//button[text()='People']")
 time.sleep(3)
 people.click()
 time.sleep(5)
-def checkconnections():
- if driver.find_element(By.XPATH,"//span[text()='Connect']"):
-    return True
- else:
-    return False
- 
-if checkconnections == True:
-   connect=driver.find_element(By.XPATH,"//span[text()='Connect']")
-   for x in range(5):
-     connect.click() 
-     time.sleep(3)
-     driver.execute_script("document.activeElement")
-     time.sleep(3)
-     send=driver.find_element(By.XPATH,"//button[@aria-label='Send now']")
-     time.sleep(3)
-     send.click()
-     time.sleep(3)
-     driver.execute_script("document.activeElement")
 
+
+all_buttons= driver.find_elements(By.TAG_NAME,"button")
+connect_buttons=[btn for btn in all_buttons if btn.text == "Connect"]
+
+if connect_buttons:
+   for btn in connect_buttons:
+    btn.click()
+    driver.execute_script("arguments[0].click();", btn)
+    time.sleep(3)
+    send=driver.find_element(By.XPATH,"//button[@aria-label='Send now']")
+    time.sleep(3)
+    send.click()
+    time.sleep(3)
+    driver.execute_script("document.activeElement")
+   driver.execute_script("window.scrollBy(0,1000)","")
+   time.sleep(10)
+   next = driver.find_element(By.XPATH,"//button[@aria-label='Next']").click()
+   time.sleep(5)
 else:
- n=2
- number=driver.find_element(By.XPATH,"//button[@aria-label='Page 3']")
- time.sleep(5)
- number.click()
- time.sleep(5)
- if checkconnections() == True:
-      connect=driver.find_element(By.XPATH,"//span[text()='Connect']")
-      for x in range(5):
-        connect.click() 
-        time.sleep(3)
-        driver.execute_script("document.activeElement")
-        time.sleep(3)
-        send=driver.find_element(By.XPATH,"//button[@aria-label='Send now']")
-        time.sleep(3)
-        send.click()
-        time.sleep(3)
-        if driver.find_element(By.XPATH,"//button[@aria-label='Withdraw']"):
-         withdraw=driver.find_element(By.XPATH,"//button[@aria-label='Withdraw']")
-        else:
-         driver.execute_script("document.activeElement")
+  driver.execute_script("window.scrollBy(0,1000)","")
+  time.sleep(5)
+  next = driver.find_element(By.XPATH,"//button[@aria-label='Next']").click()
+  time.sleep(5)
+  all_buttons= driver.find_elements(By.TAG_NAME,"button")
+  connect_buttons=[btn for btn in all_buttons if btn.text == "Connect"]
+  if connect_buttons:
+   for btn in connect_buttons:
+    btn.click()
+    driver.execute_script("arguments[0].click();", btn)
+    time.sleep(3)
+    send=driver.find_element(By.XPATH,"//button[@aria-label='Send now']")
+    time.sleep(3)
+    send.click()
+    time.sleep(3)
+    driver.execute_script("document.activeElement")
+   driver.execute_script("window.scrollBy(0,1000)","")
+   time.sleep(10)
+   next = driver.find_element(By.XPATH,"//button[@aria-label='Next']").click()
+   time.sleep(5)
+  else:
+    driver.execute_script("window.scrollBy(0,1000)","")
+    time.sleep(5)
+    next = driver.find_element(By.XPATH,"//button[@aria-label='Next']").click()
+    time.sleep(5)
 
 
-      driver.execute_script("document.activeElement")
- else:
-     number=driver.find_element(By.XPATH,"//button[@aria-label='Page 3']")
-     time.sleep(5)
-     number.click()
-     time.sleep(5)   
 driver.quit()
-print("Automation Complete") 
 
-
-
-
-
-
+print("Automation Complete")
